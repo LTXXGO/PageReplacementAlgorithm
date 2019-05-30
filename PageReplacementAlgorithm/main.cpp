d978126eadc86f3cea9040efa2fd954e3d29fa53
LTXX <ltxxgo@icloud.com>
2019-05-30 14:32:21 +0800

    Initial Commit

---
+++ PageReplacementAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PageReplacementAlgorithm
+//
+//  Created by LTXX on 2019/5/30.
+//  Copyright © 2019 LTXX. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
